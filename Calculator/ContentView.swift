8a4ac731b963bdadc36cb0deb5f3643efb9e2075
NewApp <MSP>
2023-10-03 07:38:48 +0200

    gffgf

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Kalkulator")
+            Text("Kalkulator 1")
         }
         .padding()
     }
